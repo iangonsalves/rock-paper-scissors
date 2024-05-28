c173a93de713f864d04d5bcbc8851b86b246dc08
Ian <iangonsalves29@gmail.com>
2024-05-29 00:16:35 +0400

    styling and format update

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 var humanScore = 0;
 var computerScore = 0;
 
-function playGame(){
+function playGame(humanChoice, computerChoice){
     function playRound(humanChoice, computerChoice){
         if (humanChoice === computerChoice) {
             console.log("It's a draw");
@@ -21,6 +21,8 @@ function playGame(){
             console.log(`You lose: ${computerChoice} beats ${humanChoice}`);
             computerScore++;
         }
+
+        updateChoices(humanChoice, computerChoice);
     
         return `You: ${humanScore} \n Computer: ${computerScore}`;
     }
@@ -41,6 +43,43 @@ function playGame(){
 
 }
 
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice) {
+        console.log("It's a draw");
+    }
+
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+        ) {
+            console.log(`You win: ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+    }
+
+    else {
+        console.log(`You lose: ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+
+    updateChoices(humanChoice, computerChoice);
+
+    // return `You: ${humanScore} \n Computer: ${computerScore}`;
+}
+
+
+//HELPER FUNCTIONS
+
+function updateChoices(humanChoice, computerChoice){
+    document.getElementById("player_choice").textContent = humanChoice.toUpperCase();
+    document.getElementById("computer_choice").textContent = computerChoice.toUpperCase();
+}
+
+
+
+
+
+
 
 function getComputerChoice(){
     var choices = ["rock", "paper", "scissors"];
@@ -49,14 +88,43 @@ function getComputerChoice(){
 }
 
 
-function getHumanChoice(){
-   var humanChoice = prompt("Please give us your option between Rock , Paper or Scissors").toLowerCase();
+// function getHumanChoice(){
+//    var humanChoice = prompt("Please give us your option between Rock , Paper or Scissors").toLowerCase();
 
-   if( (humanChoice == "rock" )|| (humanChoice == "paper") || (humanChoice == "scissors")){
-    console.log("You chose " + humanChoice);
-    return humanChoice;
-   } else {
-    console.log("Option doesn't exist. Please choose again between Rock , Paper, Scissors");
-    return getHumanChoice();
-    }   
+//    if( (humanChoice == "rock" )|| (humanChoice == "paper") || (humanChoice == "scissors")){
+//     console.log("You chose " + humanChoice);
+//     return humanChoice;
+//    } else {
+//     console.log("Option doesn't exist. Please choose again between Rock , Paper, Scissors");
+//     return getHumanChoice();
+//     }   
+// }
+
+
+
+
+
+
+
+
+const gameButtons = document.querySelector(".button_section");
+if(gameButtons){
+    console.log("Hello");
+} else {
+    console.log("Empty query no value");
 }
+
+gameButtons.addEventListener('click', function (event) {
+    switch (event.target.parentElement.id) {
+        case 'rock':
+            playRound('rock', getComputerChoice());
+            break;
+        case 'paper':
+            playRound('paper', getComputerChoice());
+            break;
+        case 'scissors':
+            playRound('scissors', getComputerChoice());
+            break;
+    }
+
+});
