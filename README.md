0cd940b8da5e0fb577cbd8621a188960dbdef493
Ian <iangonsalves29@gmail.com>
2024-05-30 13:58:58 +0400

    Updated Game logic +documentation.
    Added the remaining game logic, reset button and documentation for it all.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rock-paper-scissors
+# Rock-Paper-Scissors
 Repo for A simple JS project.
+
+## How to demo?
+To be able to demo the page please open:
+
+ Open the **index.html** file on your browser.
+ 
+ Page preview will look like this:
+
+ ## Page Preview:
+
+ ![Game View](/images/preview_image.png)
--- game.js
+++ game.js
@@ -1,51 +1,67 @@
 
+//global variables
 var humanScore = 0;
 var computerScore = 0;
 
-function playGame(humanChoice, computerChoice){
-    function playRound(humanChoice, computerChoice){
-        if (humanChoice === computerChoice) {
-            console.log("It's a draw");
-        }
-    
-        else if (
-            (humanChoice === "rock" && computerChoice === "scissors") ||
-            (humanChoice === "paper" && computerChoice === "rock") ||
-            (humanChoice === "scissors" && computerChoice === "paper")
-            ) {
-                console.log(`You win: ${humanChoice} beats ${computerChoice}`);
-                humanScore++;
-        }
-    
-        else {
-            console.log(`You lose: ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-
+/**
+ * Generates the scoring and choices of the game per round for display.
+ * @param {*} humanChoice  choice of the user.
+ * @param {*} computerChoice randomly generated choice of computer.
+ */
+function playRound(humanChoice, computerChoice){
+   
+    if(humanScore === 5  || computerScore ===5){
+        getFinalResult();
+    } else {
         updateChoices(humanChoice, computerChoice);
-    
-        return `You: ${humanScore} \n Computer: ${computerScore}`;
-    }
 
-    for (let i = 1; i <= 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice())
-        console.log(`Round ${i}/5 is complete. Scores are: You(${humanScore}) / Computer(${computerScore}).`)
-    }
+        const roundOutcomeText=  getRoundOutcomeText(humanChoice,computerChoice);
+        const roundOutcomeTextColor = (roundOutcomeText.includes("You win")) ? 'green' : 'red' ;
 
-    
-    if (humanScore > computerScore) {
-        console.log(`Final Score: You won! You scored ${humanScore}/5 points!`);
+        updateRoundOutcomeDisplay(roundOutcomeText, roundOutcomeTextColor);
+        updateScoreDisplay();
     }
-    else {
-        var result =  (humanScore === computerScore) ? `Final Score: You drew ! Your score was ${humanScore}/5 points.` : `Final Score: You lost ! Your scored ${humanScore}/5 points.`;
-        console.log(result);
-    }
+}
+
+/**
+ * Used to reset all game content for a new game.
+ */
+function resetGame(){
+    humanScore = 0;
+    computerScore = 0;
+
+    updateScoreDisplay();
+
+    const choiceElements = ['player_choice', 'computer_choice'];
+    const outcomeElement = document.getElementById('outcome_text');
+
+    choiceElements.forEach(choiceId => {
+        document.getElementById(choiceId).textContent = 'Null';
+    });
+
+    outcomeElement.textContent = 'Result: Null';
 
 }
 
-function playRound(humanChoice, computerChoice){
+/**
+ * Returns randomly generated computer choice.
+ * @returns computer choice
+ */
+function getComputerChoice(){
+    var choices = ["rock", "paper", "scissors"];
+    var randomChoiceNumber = Math.floor(Math.random()*choices.length);
+    return choices[randomChoiceNumber];
+}
+
+/**
+ * Logic to obtain the round winner, loser or tie.
+ * @param {*} humanChoice choice to be evaluated.
+ * @param {*} computerChoice choice to be evaluated.
+ * @returns the text to be displayed based on round outcome.
+ */
+function getRoundOutcomeText(humanChoice, computerChoice){
     if (humanChoice === computerChoice) {
-        console.log("It's a draw");
+        return "Result: It's a draw! " ;
     }
 
     else if (
@@ -53,69 +69,64 @@ function playRound(humanChoice, computerChoice){
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
         ) {
-            console.log(`You win: ${humanChoice} beats ${computerChoice}`);
             humanScore++;
-    }
+            return `Result: You win! ${humanChoice} beats ${computerChoice}`;
+        }
 
     else {
-        console.log(`You lose: ${computerChoice} beats ${humanChoice}`);
         computerScore++;
+        return `Result: You lose! ${computerChoice} beats ${humanChoice}`;
     }
-
-    updateChoices(humanChoice, computerChoice);
-
-    // return `You: ${humanScore} \n Computer: ${computerScore}`;
 }
 
-
 //HELPER FUNCTIONS
-
+/**
+ * Updates the Display choices.
+ * @param {*} humanChoice to be displayed.
+ * @param {*} computerChoice to be displayed.
+ */
 function updateChoices(humanChoice, computerChoice){
     document.getElementById("player_choice").textContent = humanChoice.toUpperCase();
     document.getElementById("computer_choice").textContent = computerChoice.toUpperCase();
 }
 
-
-
-
-
-
-
-function getComputerChoice(){
-    var choices = ["rock", "paper", "scissors"];
-    var randomChoiceNumber = Math.floor(Math.random()*choices.length);
-    return choices[randomChoiceNumber].toLowerCase();
+/**
+ * Updates the scores based on the round winner.
+ */
+function updateScoreDisplay(){
+    document.getElementById("player_score").textContent = humanScore;
+    document.getElementById("computer_score").textContent = computerScore;
 }
 
-
-// function getHumanChoice(){
-//    var humanChoice = prompt("Please give us your option between Rock , Paper or Scissors").toLowerCase();
-
-//    if( (humanChoice == "rock" )|| (humanChoice == "paper") || (humanChoice == "scissors")){
-//     console.log("You chose " + humanChoice);
-//     return humanChoice;
-//    } else {
-//     console.log("Option doesn't exist. Please choose again between Rock , Paper, Scissors");
-//     return getHumanChoice();
-//     }   
-// }
-
-
-
-
-
-
-
-
-const gameButtons = document.querySelector(".button_section");
-if(gameButtons){
-    console.log("Hello");
-} else {
-    console.log("Empty query no value");
+/**
+ * Updates the Display for round outcome.
+ * @param {*} roundOutcomeText the text used to be updated.
+ * @param {*} roundOutcomeTextColor the color of text to be updated.
+ */
+function updateRoundOutcomeDisplay(roundOutcomeText, roundOutcomeTextColor){
+    const roundOutcomeInfo = document.getElementById("outcome_text");
+    roundOutcomeInfo.textContent = roundOutcomeText;
+    roundOutcomeInfo.style.color = roundOutcomeTextColor;
 }
+
+/**
+ * Updates the final outcome based on game winner(first to 5 points).
+ */
+function getFinalResult(){
+    if( humanScore  === 5 && computerScore !==5 ){
+        document.getElementById("outcome_text").textContent = "Game Over : Congratulations, You win!";
+    } else{
+        document.getElementById("outcome_text").textContent = "Game Over : Unfortunately, Computer wins."
+       
+    }
+}
+
+//DOM content to update game.
+const gameButtons = document.querySelector(".button_container");
+const resetButton = document.getElementById("reset_button");
 
 gameButtons.addEventListener('click', function (event) {
-    switch (event.target.parentElement.id) {
+    switch (event.target.id || event.target.parentElement.id) {
         case 'rock':
             playRound('rock', getComputerChoice());
             break;
@@ -128,3 +139,5 @@ gameButtons.addEventListener('click', function (event) {
     }
 
 });
+
+resetButton.addEventListener("click", resetGame);
